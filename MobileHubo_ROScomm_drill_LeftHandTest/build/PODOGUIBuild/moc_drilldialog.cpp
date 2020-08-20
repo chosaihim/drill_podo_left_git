15daf7335682be33fb0e5c633e39aaf72d5a731a
chosaihim <chosaihim@gmail.com>
2020-08-20 10:27:07 +0900

    Mobile Hubo drilling mission-left Hand test ver1

---
+++ MobileHubo_ROScomm_drill_LeftHandTest/build/PODOGUIBuild/moc_drilldialog.cpp
@@ -0,0 +1,223 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'drilldialog.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../src/PODOGUI/drilldialog.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'drilldialog.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_DrillDialog_t {
+    QByteArrayData data[27];
+    char stringdata0[592];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_DrillDialog_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_DrillDialog_t qt_meta_stringdata_DrillDialog = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "DrillDialog"
+QT_MOC_LITERAL(1, 12, 23), // "on_PB_WalkReady_clicked"
+QT_MOC_LITERAL(2, 36, 0), // ""
+QT_MOC_LITERAL(3, 37, 26), // "on_PB_Gripper_open_clicked"
+QT_MOC_LITERAL(4, 64, 27), // "on_PB_Gripper_grasp_clicked"
+QT_MOC_LITERAL(5, 92, 27), // "on_PB_Hold_Approach_clicked"
+QT_MOC_LITERAL(6, 120, 24), // "on_PB_Hold_Grasp_clicked"
+QT_MOC_LITERAL(7, 145, 24), // "on_PB_Hold_Bring_clicked"
+QT_MOC_LITERAL(8, 170, 25), // "on_PB_Drill_Ready_clicked"
+QT_MOC_LITERAL(9, 196, 28), // "on_PB_Drill_Approach_clicked"
+QT_MOC_LITERAL(10, 225, 24), // "on_PB_Drill_Down_clicked"
+QT_MOC_LITERAL(11, 250, 22), // "on_PB_Drill_Up_clicked"
+QT_MOC_LITERAL(12, 273, 22), // "on_PB_Put_Lift_clicked"
+QT_MOC_LITERAL(13, 296, 21), // "on_PB_Put_Put_clicked"
+QT_MOC_LITERAL(14, 318, 22), // "on_PB_Put_WKRD_clicked"
+QT_MOC_LITERAL(15, 341, 18), // "on_PB_STOP_clicked"
+QT_MOC_LITERAL(16, 360, 20), // "on_PB_getCOM_clicked"
+QT_MOC_LITERAL(17, 381, 18), // "on_PB_Hold_clicked"
+QT_MOC_LITERAL(18, 400, 22), // "on_PB_Drilling_clicked"
+QT_MOC_LITERAL(19, 423, 17), // "on_PB_Put_clicked"
+QT_MOC_LITERAL(20, 441, 20), // "on_PB_HandUp_clicked"
+QT_MOC_LITERAL(21, 462, 22), // "on_PB_Approach_clicked"
+QT_MOC_LITERAL(22, 485, 18), // "on_PB_Push_clicked"
+QT_MOC_LITERAL(23, 504, 18), // "on_PB_Pull_clicked"
+QT_MOC_LITERAL(24, 523, 21), // "on_PB_Release_clicked"
+QT_MOC_LITERAL(25, 545, 22), // "on_PB_HalfOpen_clicked"
+QT_MOC_LITERAL(26, 568, 23) // "on_PB_QuatClose_clicked"
+
+    },
+    "DrillDialog\0on_PB_WalkReady_clicked\0"
+    "\0on_PB_Gripper_open_clicked\0"
+    "on_PB_Gripper_grasp_clicked\0"
+    "on_PB_Hold_Approach_clicked\0"
+    "on_PB_Hold_Grasp_clicked\0"
+    "on_PB_Hold_Bring_clicked\0"
+    "on_PB_Drill_Ready_clicked\0"
+    "on_PB_Drill_Approach_clicked\0"
+    "on_PB_Drill_Down_clicked\0"
+    "on_PB_Drill_Up_clicked\0on_PB_Put_Lift_clicked\0"
+    "on_PB_Put_Put_clicked\0on_PB_Put_WKRD_clicked\0"
+    "on_PB_STOP_clicked\0on_PB_getCOM_clicked\0"
+    "on_PB_Hold_clicked\0on_PB_Drilling_clicked\0"
+    "on_PB_Put_clicked\0on_PB_HandUp_clicked\0"
+    "on_PB_Approach_clicked\0on_PB_Push_clicked\0"
+    "on_PB_Pull_clicked\0on_PB_Release_clicked\0"
+    "on_PB_HalfOpen_clicked\0on_PB_QuatClose_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_DrillDialog[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      25,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  139,    2, 0x08 /* Private */,
+       3,    0,  140,    2, 0x08 /* Private */,
+       4,    0,  141,    2, 0x08 /* Private */,
+       5,    0,  142,    2, 0x08 /* Private */,
+       6,    0,  143,    2, 0x08 /* Private */,
+       7,    0,  144,    2, 0x08 /* Private */,
+       8,    0,  145,    2, 0x08 /* Private */,
+       9,    0,  146,    2, 0x08 /* Private */,
+      10,    0,  147,    2, 0x08 /* Private */,
+      11,    0,  148,    2, 0x08 /* Private */,
+      12,    0,  149,    2, 0x08 /* Private */,
+      13,    0,  150,    2, 0x08 /* Private */,
+      14,    0,  151,    2, 0x08 /* Private */,
+      15,    0,  152,    2, 0x08 /* Private */,
+      16,    0,  153,    2, 0x08 /* Private */,
+      17,    0,  154,    2, 0x08 /* Private */,
+      18,    0,  155,    2, 0x08 /* Private */,
+      19,    0,  156,    2, 0x08 /* Private */,
+      20,    0,  157,    2, 0x08 /* Private */,
+      21,    0,  158,    2, 0x08 /* Private */,
+      22,    0,  159,    2, 0x08 /* Private */,
+      23,    0,  160,    2, 0x08 /* Private */,
+      24,    0,  161,    2, 0x08 /* Private */,
+      25,    0,  162,    2, 0x08 /* Private */,
+      26,    0,  163,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void DrillDialog::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        DrillDialog *_t = static_cast<DrillDialog *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_PB_WalkReady_clicked(); break;
+        case 1: _t->on_PB_Gripper_open_clicked(); break;
+        case 2: _t->on_PB_Gripper_grasp_clicked(); break;
+        case 3: _t->on_PB_Hold_Approach_clicked(); break;
+        case 4: _t->on_PB_Hold_Grasp_clicked(); break;
+        case 5: _t->on_PB_Hold_Bring_clicked(); break;
+        case 6: _t->on_PB_Drill_Ready_clicked(); break;
+        case 7: _t->on_PB_Drill_Approach_clicked(); break;
+        case 8: _t->on_PB_Drill_Down_clicked(); break;
+        case 9: _t->on_PB_Drill_Up_clicked(); break;
+        case 10: _t->on_PB_Put_Lift_clicked(); break;
+        case 11: _t->on_PB_Put_Put_clicked(); break;
+        case 12: _t->on_PB_Put_WKRD_clicked(); break;
+        case 13: _t->on_PB_STOP_clicked(); break;
+        case 14: _t->on_PB_getCOM_clicked(); break;
+        case 15: _t->on_PB_Hold_clicked(); break;
+        case 16: _t->on_PB_Drilling_clicked(); break;
+        case 17: _t->on_PB_Put_clicked(); break;
+        case 18: _t->on_PB_HandUp_clicked(); break;
+        case 19: _t->on_PB_Approach_clicked(); break;
+        case 20: _t->on_PB_Push_clicked(); break;
+        case 21: _t->on_PB_Pull_clicked(); break;
+        case 22: _t->on_PB_Release_clicked(); break;
+        case 23: _t->on_PB_HalfOpen_clicked(); break;
+        case 24: _t->on_PB_QuatClose_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject DrillDialog::staticMetaObject = {
+    { &QDialog::staticMetaObject, qt_meta_stringdata_DrillDialog.data,
+      qt_meta_data_DrillDialog,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *DrillDialog::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *DrillDialog::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_DrillDialog.stringdata0))
+        return static_cast<void*>(const_cast< DrillDialog*>(this));
+    return QDialog::qt_metacast(_clname);
+}
+
+int DrillDialog::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 25)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 25;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 25)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 25;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
